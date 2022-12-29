36afa84eaea930190e65e0096ad6c34e7685f12d
Khuyet-Hung <94001464+Khuyet-Hung@users.noreply.github.com>
2022-12-29 15:47:53 +0700

    Add new vanilla clock project

---
+++ assets/js/vanilla-clock.js
@@ -0,0 +1,45 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const minute = $$('.minute')[0];
+const second = $$('.second')[0];
+const millisecond = $$('.millisecond')[0];
+const startBtn = $('#btnStart');
+const stopBtn = $('#btnStop');
+const resetBtn = $('#btnReset');
+
+var milliseconds = 0;
+var seconds = 0;
+var minutes = 0;
+
+startBtn.onclick = function () {
+    var myClock = setInterval(function () {
+        if (milliseconds === 100) {
+            seconds += 1;
+            milliseconds = 0;
+            if (seconds === 2) {
+                minutes += 1;
+                seconds = 0;
+            }
+        } else {
+            milliseconds += 1;
+        }
+        millisecond.innerText = milliseconds >= 10 ? String(milliseconds) : '0' + String(milliseconds);
+        second.innerText = seconds >= 10 ? String(seconds) + ':' : '0' + String(seconds) + ':';
+        minute.innerText = minutes >= 10 ? String(minutes) + ':' : '0' + String(minutes) + ':';
+    }, 10);
+
+
+    stopBtn.onclick = function () {
+        clearInterval(myClock);
+    }
+    resetBtn.onclick = function () {
+        clearInterval(myClock);
+        milliseconds = 0;
+        seconds = 0;
+        minutes = 0;
+        millisecond.innerText = milliseconds >= 10 ? String(milliseconds) : '0' + String(milliseconds);
+        second.innerText = seconds >= 10 ? String(seconds) + ':' : '0' + String(seconds) + ':';
+        minute.innerText = minutes >= 10 ? String(minutes) + ':' : '0' + String(minutes) + ':';
+    }
+}
